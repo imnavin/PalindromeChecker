b42473208927a7a3a64f7e4999dcab9b094f7fb7
Harshliyadipita <liyadipita16@gmail.com>
2017-06-03 05:43:23 -0700

    coded the stack class

--- app/src/main/java/com/example/we21/palindromechecker/Stack.java
+++ app/src/main/java/com/example/we21/palindromechecker/Stack.java
@@ -5,4 +5,47 @@ package com.example.we21.palindromechecker;
  */
 
 public class Stack {
+    private int MaxSize;
+    private int top;
+    private char StackArray[];
+
+    public Stack(int s) {
+        MaxSize = s;
+        top = -1;
+        StackArray = new char[MaxSize];
+    }
+
+    public boolean isFull(){
+        return (top+1==MaxSize);
+    }
+
+    public void push(char V){
+        if(!isFull()){
+            StackArray[++top]=V;
+        }
+        else{
+            System.out.println("Array is full");
+        }
+    }
+
+    public boolean isEmpty(){
+        return (top==-1);
+    }
+
+    public char pop(){
+        if(!isEmpty()){
+            return StackArray[top--];
+        }
+        else{
+            return 'o';
+        }
+    }
+    public char peak(){
+        if(!isEmpty()){
+            return StackArray[top];
+        }
+        else{
+            return 'o';
+        }
+    }
 }
